0b3469186eedac892654440ecc26d0648b4f44c5
Alex-Devonte <alexdevontetaylor@gmail.com>
2017-10-19 16:30:14 -0400

    Added code to reset form and fade out success msg.
    Added in some spacing for extra readability.

--- save_inquiry.js
+++ save_inquiry.js
@@ -1,38 +1,51 @@
 $(function() { 
   var submit = $("#submit");
   var contactForm = $("#contact-form");
-
+  
   submit.on("click", function(e) {
     resetErrors();
+    
     //Prevent the form from submitting
     e.preventDefault();
+    
     //Make a call to ajax to error check the form and submit if no errors
     $.ajax({
       type: "POST",
-      url: "save_inquiry.php",
+      url: "SubmitForm.php",
       data: contactForm.serialize(),
       success: function(result) {
-       //Turn the result into an object
+        //Turn the result into an object
         result = JSON.parse(result);
+        
         //Get the number of values in the object
         var resultLength = Object.values(result).length;
         
         //If the array isn't empty, iterate through the JSON object and place errors next to the appropriate field
-        if (resultLength <= 4) {
+        if (resultLength <= 4)
+        {
           $.each(result, function(k,v) {
             var errorMsg = "<label for='" + k + "'class='form-error'>" + v + "</label>";
             $("input[name='" + k + "']").addClass("input-error").after(errorMsg);
           });
         }
         else
-        {
+        { 
+          var successMsg = $("<p>" + result + "</p>");
+          
           //Display success message
-          submit.before(result);
+          submit.before(successMsg);
+
+          //Fade the success message out
+          successMsg.fadeOut(5000, "linear");
+          
+          //Reset form fields
+          contactForm.trigger('reset');
         }
       }
     });
   });
 });
+
 /**
  *
  *resetErrors()
